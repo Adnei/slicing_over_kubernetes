e8e7f6cac3aeaa7069c9e86fe0a54c20c8a12408
adnei <adneiwillian@gmail.com>
2024-02-29 16:39:08 -0300

    working example - using api to list pods

--- remote_access.py
+++ remote_access.py
@@ -1,8 +1,12 @@
+# Load venv from kubernetes package
+
 from kubernetes import client, config
 
 
 def main():
-    auth_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImFabnJBcXBxcngyTVFmOFVZVGdyOURRSzQ1a3d0TFpuTkdMRGZmelpxT3cifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImRlZmF1bHQtdG9rZW4iLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiZGVmYXVsdCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjRlMzkwYzE2LWFhZGQtNDRmNi05YWYyLWFmYzIwZDY3NTJiYiIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OmRlZmF1bHQifQ.W_HnTmeOxwv1iMClo0lRSWecPSANVU6FayKT9yFOzq5rVW-wCtV-jOIUIjvH2Vi8lxJ1DE-mjBi6_oUmw_1tTFz1Usd0D3CedicnhJ6aQTFftwPzdtAKBFgqvYxnH54YUloWhz4wAkNNQQEIAtLLDUXAfTr7eURCvV6zZvH3pDR8DGf-RQwNbRwEi2XOsb7HVCQY5kGxm1rXnmF82k20-EPNOudljKEzbMb6U4cwNIpnNlR5aebHJd7OulDv5RGbVC8_fYgKFtWC7GhQ2OqRtSAyldT-mE_E_SrNFPGmyDk6LY9DkPHTqUbSGIhYu0YppX93hZUkrsYuIVBynFOwjg"
+    auth_token = (
+        "<get token with: kubectl describe secret -n api-slicing api-slicing-secret>"
+    )
     client_config = client.Configuration()
     client_config.host = "https://10.10.225.91:6443"
     # no ssl security for now
@@ -20,3 +24,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# follow :
+# Principal --> https://stackoverflow.com/questions/62589073/kubernetes-api-cannot-list-resource-pods-in-api-group?rq=3
+# criar namespace (talvez), ServiceAccount, Role, and RoleBinding para API
+# https://stackoverflow.com/questions/55742540/kubernetes-python-client-connection-issue
+#
